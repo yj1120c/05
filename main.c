97f8b564d33dfd3afcfa5b50338fcdac78c0115d
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-10-06 16:13:54 +0900

    03

--- main.c
+++ main.c
@@ -5,16 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int num=0;
+	char c;
 	
-	printf("정수 하나를 입력하시오.:");
-	scanf("%i", &i);
+	printf("Input a string:");
 	
-	if (i < 0)
-	    i = i*(-1);
-	    
-	printf("절대값은 %i 입니다.\n", i);
+	while( (c=getchar()) != '\n')
+	{
+		if ( c >= '0' && c <= '9')
+		{
+			num++;
+		}
+	}
 	
+	printf("the number of digits is %i\n", num);
 	
 	return 0;
 }
