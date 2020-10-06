98706ce6a8b989fc199399456a80eb5f5199f2c4
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-10-06 16:59:09 +0900

    05

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i,n;
-	int sum=0;
+	int answer = 59;
+	int i, trial=0;
 	
-	printf("input a number:");
-	scanf("%i", &n);
+	do
+	{
+		printf("input a number:");
+		scanf("%i", &i);
+		
+		if (answer > i)
+		    printf("low!\n");
+		else if (answer < i)
+		    printf("high!\n");
 	
-	for (i=1;i<=n;i++)
-	    sum += i;//summation
+	    trial++;
+	} while(i != answer);
 	
-	printf("the sum result is %i\n", sum);
+	printf("Conglaturation, number of trial : %i\n", trial);
 	
 	return 0;
 }
